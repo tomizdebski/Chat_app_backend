1a10ce3cb4661780193b0c66fbe17f521b94ef3f
tomizdebski <tomizdebski@gmail.com>
2023-09-27 11:11:48 +0200

    add_gitignore

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
