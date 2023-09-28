1cc4fbb872a5447f8d5fde865cf0454dba7ebc4b
tomizdebski <tomizdebski@gmail.com>
2023-09-28 14:58:16 +0200

    update11

--- index.js
+++ index.js
@@ -2,12 +2,39 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
+const http = require('http').Server(app);
+const cors = require('cors');
+
+app.use(cors());
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+
+    socket.on('message', (data) => {
+        console.log(data);
+        socketIO.emit('messageResponse', data);
+      });
+
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected');
+      socketIO.emit('messageResponse', data);
+    });
+});
+
+
 app.get('/api', (req, res) => {
   res.json({
     message: 'Hello world',
   });
 });
 
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
