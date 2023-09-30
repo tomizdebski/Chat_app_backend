29220a4783892ee6275cf984fbcc9c61caa2cbfe
tomizdebski <tomizdebski@gmail.com>
2023-09-30 18:17:47 +0200

    update_env

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
-const express = require('express');
+const express = require("express");
+require('dotenv').config()
+const fs = require('fs');
 const app = express();
 const PORT = 4000;
 
-const http = require('http').Server(app);
-const cors = require('cors');
+const http = require("http").Server(app);
+const cors = require("cors");
 
 app.use(cors());
 
-const socketIO = require('socket.io')(http, {
-    cors: {
-        origin: "http://localhost:3000"
-    }
+const rawData = fs.readFileSync('messages.json');
+const messagesData = JSON.parse(rawData);
+
+
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: process.env.URL_SOCKET,
+  },
 });
 
+let users = [];
 
-socketIO.on('connection', (socket) => {
-    console.log(`⚡: ${socket.id} user just connected!`);
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
 
-    socket.on('message', (data) => {
-        console.log(data);
-        socketIO.emit('messageResponse', data);
-      });
+  socket.on("message", data => {
+    messagesData["messages"].push(data)
+    const stringData = JSON.stringify(messagesData, null, 2)
+    fs.writeFile("messages.json", stringData, (err)=> {
+      console.error(err)
+    })
+    socketIO.emit("messageResponse", data)
+  })
 
-    socket.on('disconnect', () => {
-      console.log('🔥: A user disconnected');
-      socketIO.emit('messageResponse', data);
-    });
+  socket.on('typing', (data) => socket.broadcast.emit('typingResponse', data));
+
+  socket.on("newUser", (data) => {
+    
+    users.push(data);
+    console.log(users);
+   
+    socketIO.emit("newUserResponse", users);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('🔥: A user disconnected');
+    
+    users = users.filter((user) => user.socketID !== socket.id);
+    
+    socketIO.emit('newUserResponse', users);
+    socket.disconnect();
+  });
 });
-
 
 app.get('/api', (req, res) => {
-  res.json({
-    message: 'Hello world',
-  });
+  res.json(messagesData);
 });
 
 http.listen(PORT, () => {
